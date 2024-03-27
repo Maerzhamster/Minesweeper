506e6492316f15d55f33f24e9edd4f57a068758f
Oliver Hohlstein <Oliver.Hohlstein@gft.com>
2024-03-27 11:47:44 +0100

    Löschen von unbenutzten using-Einträge

--- Minesweeper/GameData.cs
+++ Minesweeper/GameData.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using static System.Random;
-
-namespace Minesweeper
+﻿namespace Minesweeper
 {
     /// <summary>
     /// the game Data
